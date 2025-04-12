c215fb8dafd2e25cdad8219926faa7d8d6d038e3
Abdullah Ayoola <officialayo540@gmail.com>
2025-04-12 10:04:35 +0100

    added resume to get profile

--- src/controllers/seeker/seeker.resumemgmt.controller.ts
+++ src/controllers/seeker/seeker.resumemgmt.controller.ts
@@ -1,8 +1,6 @@
 import { Response } from "express";
 import { IUserRequest } from "../../interface";
 import cloudinary from "../../utils/cloudinaryConfig";
-import fs from "fs";
-import path from "path";
 import User from "../../models/users.model";
 import { handleErrors } from "../../helper/handleErrors";
 import { Readable } from "stream";
@@ -16,30 +14,6 @@ const uploadResume = async function (req: IUserRequest, res: Response) {
 
     const user = await User.findById(userId);
     if (!user) return res.status(404).json({ message: "User not found!" });
-
-    // Construct full file path
-    // const filePath = path.join(__dirname, "../../../uploads", resume.filename);
-
-    // // Ensure file exists
-    // if (!fs.existsSync(filePath)) {
-    //   return res.status(500).json({ error: "File not found after upload" });
-    // }
-
-    // const response = await cloudinary.uploader.upload_stream(resume, {
-    //   folder: `users/${role}/${userId}/resume`,
-    //   resource_type: "auto",
-    // });
-
-    // user.resume = response.secure_url;
-    // await user.save();
-
-    // ✅ Delete local file after successful upload
-    // fs.unlink(filePath, err => {
-    //   if (err) console.error("Error deleting file:", err);
-    //   else console.log("File deleted successfully:", filePath);
-    // });
-
-    // res.status(200).json({ message: "Resume Upload Success", url: response.secure_url });
 
     // Upload file directly to Cloudinary using the buffer
     const stream = cloudinary.uploader.upload_stream(
--- src/utils/multerConfig.ts
+++ src/utils/multerConfig.ts
@@ -1,20 +1,4 @@
 import multer from "multer";
-import fs from "fs";
-import path from "path";
-
-// const storage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     const uploadDir = path.join(__dirname, "uploads");
-//     if (!fs.existsSync(uploadDir)) {
-//       fs.mkdirSync(uploadDir, { recursive: true });
-//     }
-
-//     cb(null, "uploads/");
-//   },
-//   filename: (req, file, cb) => {
-//     cb(null, `${Date.now()}_${file.originalname}`);
-//   },
-// });
 
 const storage = multer.memoryStorage();
 
