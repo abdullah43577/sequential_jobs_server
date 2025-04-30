13bd1129da0d4a8b09cab31287c6a37a46a39e0a
Abdullah Ayoola <officialayo540@gmail.com>
2025-04-30 12:07:13 +0100

    code update, added logo for auth emails and as well as using the baseurl to

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -81,7 +81,7 @@ const validateEmail = async (req: Request, res: Response) => {
 
     await transportMail({ email: user.email, subject: "Welcome to Sequential Jobs", message: html.html });
 
-    res.redirect(`${getBaseUrl}/auth/email-activation-success?name=${encodeURIComponent(user.first_name)}`);
+    res.redirect(`https://sequential-jobs.vercel.app/auth/email-activation-success?name=${encodeURIComponent(user.first_name)}`);
   } catch (error) {
     handleErrors({ res, error });
   }
