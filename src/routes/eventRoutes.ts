05f8f39e5a7ecb871aa4a2c88581c359ef5befb9
Abdullah Ayoola <officialayo540@gmail.com>
2025-04-30 08:01:15 +0100

    code update worked on the events workflows of the application

--- src/routes/eventRoutes.ts
+++ src/routes/eventRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { validateAccessToken, validateCompanySession } from "../middleware/validateToken";
-import { getEmployerEvents } from "../controllers/events.controller";
+import { validateAccessToken, validateCompanySession, validateSeekerSession } from "../middleware/validateToken";
+import { getEmployerEvents, getSeekerEvents } from "../controllers/events.controller";
 
 const eventsRouter = Router();
 
 eventsRouter.get("/company_events", validateAccessToken, validateCompanySession, getEmployerEvents);
+eventsRouter.get("/seeker_events", validateAccessToken, validateSeekerSession, getSeekerEvents);
 
 export { eventsRouter };
