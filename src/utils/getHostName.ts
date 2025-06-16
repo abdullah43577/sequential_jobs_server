e17255f1c343662bc620f6afe4f3e3fb31a982e2
Abdullah Ayoola <officialayo540@gmail.com>
2025-06-16 12:50:24 +0100

    made modifications for jobs returned ffor the US

--- src/utils/getHostName.ts
+++ src/utils/getHostName.ts
@@ -1,30 +1,50 @@
-import { Request, Response } from "express";
+import { Request } from "express";
 
 interface SubdomainToCountry {
-  ng: string;
-  gh: string;
-  ae: string;
-  gb: string;
-  ca: string;
-  ke: string;
-  ph: string;
+  gb: string; // UK
+  ca: string; // Canada
+  ae: string; // UAE
+  ng: string; // Nigeria
+  za: string; // South Africa
+  ke: string; // Kenya
+  gh: string; // Ghana
+  ug: string; // Uganda
+  tz: string; // Tanzania
+  ma: string; // Morocco
+  tn: string; // Tunisia
+  dz: string; // Algeria
+  zw: string; // Zimbabwe
+  ph: string; // Philippines
 }
 
 export const getDomainHost = function (req: Request) {
   // Get the hostname from the request
   const hostname = req.headers.host || "";
 
+  console.log(hostname, "host name here");
+
+  if (hostname === "sequentialjobs.com") {
+    return "United States";
+  }
+
   if (hostname.includes(".sequentialjobs.com")) {
     const subdomain = hostname.split(".")[0] as keyof SubdomainToCountry;
 
     // Map subdomain to country code
     const subdomainToCountry: SubdomainToCountry = {
-      ng: "Nigeria",
-      gh: "Ghana",
-      ae: "United Arab Emirates",
       gb: "United Kingdom",
       ca: "Canada",
+      ae: "United Arab Emirates",
+      ng: "Nigeria",
+      za: "South Africa",
       ke: "Kenya",
+      gh: "Ghana",
+      ug: "Uganda",
+      tz: "Tanzania",
+      ma: "Morocco",
+      tn: "Tunisia",
+      dz: "Algeria",
+      zw: "Zimbabwe",
       ph: "Philippines",
     };
 
