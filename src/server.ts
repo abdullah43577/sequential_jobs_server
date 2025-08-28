02cc14137de244389b7b6117dec83b384504fbca
Abdullah Ayoola <officialayo540@gmail.com>
2025-08-28 03:40:44 +0100

    code update, enacted fix for bulk upload as well as ensured proper validation by the server for locations count per tier level

--- src/server.ts
+++ src/server.ts
@@ -158,7 +158,7 @@ const cronJobs = async function () {
 
 //   console.log(`Migration complete. Updated ${updatedCount} jobs.`);
 // }
-
+//
 const server = app.listen(PORT, async () => {
   try {
     console.log("Connecting to DB....");
